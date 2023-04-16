62184e3ed49ae05d12b3aea5c641aa32dd313a35
Akhil Singh Chauhan <109506738+akhil-OO7@users.noreply.github.com>
2023-04-16 21:35:13 +0530

    Create Constructor.java

---
+++ Constructor.java
@@ -0,0 +1,10 @@
+class SimpleObject{
+    SimpleObject(){
+        System.out.println("Hello World");
+    }
+}
+class HelloWorld{
+    public static void main(String[] args) {
+        SimpleObject obj1 = new SimpleObject();
+    }
+}
