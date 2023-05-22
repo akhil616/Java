d8fb32a37387bb3e89877522406e3065e014ed91
Akhil Singh Chauhan <109506738+akhil-OO7@users.noreply.github.com>
2023-05-22 14:59:13 +0530

    Create Q3-3.java

---
+++ Assignment3/Q3-3.java
@@ -0,0 +1,33 @@
+class InvalidAge extends Exception {
+
+	public InvalidAge(String string) {
+		// TODO Auto-generated constructor stub
+		super(string);
+	}
+	
+}
+
+class Age1 {
+	boolean checkAge(int age) throws InvalidAge{
+		if (age > 18) {
+			return true;
+		}
+		else
+			throw new InvalidAge("You cannot Vote");
+	}
+}
+
+public class UserDefinedException {
+
+	public static void main(String[] args) {
+		Age1 a1 = new Age1();
+		try {
+			a1.checkAge(12);
+			System.out.println("You are eligible to vote");
+		} catch (InvalidAge ia) {
+			System.out.println(ia.toString());
+		}
+		System.out.println("Rest of the Code");
+	}
+
+}
