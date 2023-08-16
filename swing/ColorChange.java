c77020ea066163fdc437cd7c6b20f30ced0e856e
Akhil Singh Chauhan <109506738+akhil-OO7@users.noreply.github.com>
2023-08-16 21:35:41 +0530

    Update ColorChange.java

--- swing/ColorChange.java
+++ swing/ColorChange.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 public class Main {
   public static void main(String[] args) {
     JFrame f = new JFrame();
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setLayout(null);
+    f.setSize(500, 500);
     
     TextField tf = new TextField();
     tf.setBounds(60, 50 180, 25);
