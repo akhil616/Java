e2efdb6a2c29e2da7f553485b5af584ea86c27b7
Akhil Singh Chauhan <109506738+akhil-OO7@users.noreply.github.com>
2023-08-16 21:34:24 +0530

    Create ColorChange.java

---
+++ swing/ColorChange.java
@@ -0,0 +1,29 @@
+import java.awt.event*;
+import javax.swing.*;
+
+public class Main {
+  public static void main(String[] args) {
+    JFrame f = new JFrame();
+    
+    TextField tf = new TextField();
+    tf.setBounds(60, 50 180, 25);
+
+    JButton button = new JButton("Click Here");
+    button.setBounds(100, 120, 80 30);
+    button.addActionListener(new ActionListener() {        //Anonymous Class
+          public void actionPerformed(ActionEvent e) {
+              tf.setText("Welcome to Java Swing");
+              f.getContentPane().setBackground(Color.RED);
+          }
+    });
+    
+    f.add(tf);
+    f.add(button);
+    f.setVisible(true);
+  }
+}    
+    
+
+    
+    
+  
