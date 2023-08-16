92ba957be8944e01c475c1ef3176380adf718f75
Akhil Singh Chauhan <109506738+akhil-OO7@users.noreply.github.com>
2023-08-16 21:20:02 +0530

    Create ActionListener.java

---
+++ swing/ActionListener.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Main implements ActionListener {
+  Main() {
+    TextField tf = new TextField();
+    tf.setBounds(60, 50, 180, 25);
+
+    JButton button = new Jbutton("Click Here");
+    button.setBounds(100, 120, 80, 30);
+    button.addActionListener(this);
+
+    add(tf);
+    add(button);
+    setVisible(true);
+  }
+
+  //implementing method of ActionListener
+  @Override
+  public void actionPerformed(ActionEvent e){
+    tf.setTextField("Hello World!");
+  }
+
+  public static void main(String[] args){
+    new Main();
+  }
