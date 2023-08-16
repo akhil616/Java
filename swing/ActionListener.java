fc1698b16673509384275d21585a8f14e9042ece
Akhil Singh Chauhan <109506738+akhil-OO7@users.noreply.github.com>
2023-08-16 21:49:39 +0530

    Update ActionListener.java

--- swing/ActionListener.java
+++ swing/ActionListener.java
@@ -2,29 +2,25 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Main implements ActionListener {
-  TextField tf;
-  JButton button;
-  Main() {
-    tf = new TextField();
-    tf.setBounds(60, 50, 180, 25);
-
-    button = new Jbutton("Click Here");
-    button.setBounds(100, 120, 80, 30);
-    button.addActionListener(this);
-
-    add(tf);
-    add(button);
-    setVisible(true);
-  }
-
-  //implementing method of ActionListener
-  @Override
-  public void actionPerformed(ActionEvent ae) {
-    tf.setText("Hello World!");
-  }
-
-  public static void main(String[] args) {
-    new Main();
+public class Main {
+    public static void main(String[] args){
+      JFrame f = new Frame();
+      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      f.setLayout(null);
+      f.setSize(500, 500);
+      
+      JTextField tf = new JTextField();
+      tf.setBounds(60, 50, 180, 25);
+  
+      button = new Jbutton("Click Here");
+      button.setBounds(100, 120, 80, 30);
+      button.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent ae) {
+          tf.setText("Hello World!");
+        }
+      });  
+      f.add(tf);
+      f.add(button);
+      f.setVisible(true);
   }
 }
